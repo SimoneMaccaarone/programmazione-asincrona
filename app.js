5ee7bfcfc3813c2edae53e511b61622eae31226e
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-14 09:31:42 +0200

    aggiunto e fixato old codes(display to do "vecchio"
    aggiunto e fixato old codes(display to do "vecchio"

--- app.js
+++ app.js
@@ -151,49 +151,54 @@ function displayTodos2(){
     }
 }
 
-
 // OLD CODES
-        //---------- OLD  TAG ----------
-        // function displayTodos() { //è globale quindi la vede
-
-        // console.log(superList);
-        // const titleSpan = document.createElement('span');
-        // titleSpan.classList.add('todo-title')
-
-        // const dateSpan = document.createElement('span');
-        // dateSpan.classList.add('todo-date');
-
-        // const titleNode = document.createTextNode(todo.title);
-        // const dateNode = document.createTextNode(todo.creationDate);
-
-
-        //--------- OLD COMPLETE & REMOVE ---------
-        //------------- COMPLETE -------------
-        // //aggiunta del tasto 'Completato' todo
-        // const completeButton = document.createElement('button');
-        // const textCompleteButton = document.createTextNode('Completato');
-        // completeButton.classList.add('complete-btn');
-
-        // completeButton.appendChild(textCompleteButton);
-
-        // completeButton.addEventListener('click', (event) => {
-        //     superList.completeTodo(todo);
-        //     displayTodos();
-        // });
-
-        // //-------------- REMOVE --------------
-        // //aggiunta del tasto 'Rimuovi' todo
-        // const removeButton = document.createElement('button');
-        // const textRemoveButton = document.createTextNode('Rimuovi');
-        // removeButton.classList.add('remove-btn');
-
-        // removeButton.appendChild(textRemoveButton);
-
-        // removeButton.addEventListener('click', (event) => {
-        //     superList.removeTodo(todo)
-        //     displayTodos();
-        // });
-        //------------------------------------
-
-        // titleSpan.appendChild(titleNode); 9u07907
-        // dateSpan.appendChild(dateNode);
+// function displayTodos() { //è globale quindi la vede
+//     console.log(superList)
+//     const todoListTitle = document.getElementById('list-name');
+//     const todoListUl = document.getElementById('todo-list');
+//     const titleNode = document.createTextNode(superList.title);
+//     todoListTitle.innerHTML = ''; // pulisci tutto quello che ce dentro a list title
+//     todoListTitle.appendChild(titleNode);
+//     todoListUl.innerHTML = '';
+//     for (let i = 0; i < superList.todoArray.length; i++) {
+//         const todo = superList.todoArray[i];
+//         const newLi = document.createElement('li');
+//         newLi.classList.add('todo-li');
+//         if(todo.isCompleted){
+//             newLi.style.backgroundColor = 'chartreuse';
+//         }
+//         const titleSpan = document.createElement('span');
+//         titleSpan.classList.add('todo-title')
+//         const dateSpan = document.createElement('span');
+//         dateSpan.classList.add('todo-date');
+//         const titleNode = document.createTextNode(todo.title);
+//         const dateNode = document.createTextNode(todo.creationDate);
+//         //------------- COMPLETE -------------
+//         const completeButton = document.createElement('button');
+//         const textCompleteButton = document.createTextNode('Completato');
+//         completeButton.classList.add('complete-btn');
+//         completeButton.appendChild(textCompleteButton);
+//         completeButton.addEventListener('click', (event) => {
+//             superList.completeTodo(todo);
+//             displayTodos();
+//         })
+//         //-------------- REMOVE --------------
+//         //aggiunta del tasto rimuovi todo
+//         const removeButton = document.createElement('button');
+//         const textRemoveButton = document.createTextNode('Rimuovi');
+//         removeButton.classList.add('remove-btn');
+//         removeButton.appendChild(textRemoveButton);
+//         removeButton.addEventListener('click', (event) => {
+//             superList.removeTodo(todo)
+//             displayTodos();
+//         });
+//         //------------------------------------
+//         titleSpan.appendChild(titleNode);
+//         dateSpan.appendChild(dateNode);
+//         newLi.appendChild(titleSpan);
+//         newLi.appendChild(dateSpan);
+//         newLi.appendChild(completeButton)
+//         newLi.appendChild(removeButton)
+//         todoListUl.appendChild(newLi);
+//     }
+// }
